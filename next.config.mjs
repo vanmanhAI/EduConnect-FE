57ea30c4fe7470003e0c4afb8b8c70ac5f5eecbf
cvm <vanmanh3c9@gmail.com>
2026-01-05 21:55:05 +0700

    fix: cors

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,7 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
-  },
-  allowedDevOrigins: ['others-monica-southampton-separated.trycloudflare.com'],
+  }
 }
 
 export default nextConfig
